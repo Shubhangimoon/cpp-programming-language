5c7a0659f3cec95c04fe1cc10b0002914287ba88
Shubhangimoon <shubhangimoon790@gmail.com>
2025-02-03 11:59:38 +0530

    Add files via upload

---
+++ day5cpp/staticdatamember.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class one
+{
+	static int a;
+	public:
+		void set()
+		{
+			a++;
+		}
+		void display()
+		{
+			cout<<a<<endl;
+		}};
+		int one::a;//scope&type
+
+int main()
+{
+one ob1,ob2,ob3;
+ob1.display();
+ob2.display();
+ob3.display();
+ob2.set();
+ob1.display();
+ob2.display();
+ob3.display();
+ob3.set();
+ob1.display();
+ob2.display();
+ob3.display();
+}
+
---
+++ day5cpp/staticmemberfunction.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class one
+{
+	int a,b;
+	static int c;
+	public:
+	void set(int x,int y)
+	{
+		a = x;
+		b = y;
+		c++;
+	}
+	void display()
+	{
+		cout<<a<<" "<<b<<" "<<c<<endl;
+	}
+	static void print()
+	{
+		cout<<c<<endl;
+	}};
+	int one::c;
+	int main()
+	{
+		one ob1;
+		ob1.set(2,3);
+		ob1.display();
+		ob1.print();
+		one::print();
+	}
+
