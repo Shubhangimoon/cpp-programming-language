76f6c682a9659057f45e7ed1cef980c91678d2c3
Shubhangimoon <shubhangimoon790@gmail.com>
2025-02-03 12:01:18 +0530

    Add files via upload

---
+++ day6cpp/destructor.cpp
@@ -0,0 +1,32 @@
+#include<string.h>
+#include<iostream>
+using namespace std;
+class one
+{
+	public:
+		char*name;
+		one(const char*input)
+		{
+			int len=strlen(input)+1;
+			name=new char[len];
+			strcpy (name,input);
+		}
+		void display()
+		{
+			cout<<name<<endl;
+	    
+		}
+		~one()
+		{
+			delete name;
+			cout<<"destructor called"<<endl;
+		}
+		
+};
+int main()
+{
+	one ob1("RAM");
+	ob1.display();
+	one ob2("SHUBHU");
+	ob2.display();
+}
---
+++ day6cpp/dynamicconstructor.cpp
@@ -0,0 +1,24 @@
+#include<string.h>
+#include<iostream>
+using namespace std;
+class one
+{
+	public:
+		char*name;
+		one(const char*input)
+		{
+			int len=strlen(input)+1;
+			name=new char[len];
+			strcpy (name,input);
+		}
+		void display()
+		{
+			cout<<name<<endl;
+	    
+		}
+};
+int main()
+{
+	one ob1("RAM");
+	ob1.display();
+}
